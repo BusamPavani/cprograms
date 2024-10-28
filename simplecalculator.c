633935e58e1baffacfe7a307591db630fb5e7ee7
BusamPavani <pavanibusam1207@gmail.com>
2024-10-28 12:03:04 +0530

    Create simplecalculator.c

---
+++ simplecalculator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    char op;
+    printf("enter the a,b values and operator");
+    scanf("%d%d %c",&a,&b,&op);
+    switch(op)
+    {
+    case'+': printf("the addition is %d",a+b);
+    break;
+    case'-': printf("the subtraction is %d",a-b);
+    break;
+    case'*': printf("the multiplication is %d",a*b);
+    break;
+    case'/': printf("the division is %d",a/b);
+    break;
+    case'%': printf("the remainder is %d",a%b);
+    break;
+    default: printf("invalid output");
+    break;
+    }
+}
